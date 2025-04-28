c2360070c3b5cb2d7237f49b3d77834640f09561
WahyuTomo14 <wahyuxd14@gmail.com>
2025-04-28 18:10:29 +0700

    setup panel dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,9 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->font('Inter')
+            ->brandName('E - Learning')
+            ->darkMode(false)
             ->login()
             ->colors([
                 'primary' => Color::Amber,
