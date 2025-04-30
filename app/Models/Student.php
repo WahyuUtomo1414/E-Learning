3f4677b9b33b4c995fbb3eb714781193649c15e9
WahyuTomo14 <wahyuxd14@gmail.com>
2025-04-30 14:07:25 +0700

    buat tabel student

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes, AuditedBySoftDelete;
+    protected $table = 'student';
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ database/migrations/2025_04_30_070557_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDelete;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('student_number');
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
