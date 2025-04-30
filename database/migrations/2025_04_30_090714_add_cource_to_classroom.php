b8f32445c8845c2ea7d1a33ff3085214a92be3f1
WahyuTomo14 <wahyuxd14@gmail.com>
2025-04-30 16:09:05 +0700

    tambah FK cource ke table classroom

---
+++ database/migrations/2025_04_30_090714_add_cource_to_classroom.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('classroom', function (Blueprint $table) {
+            $table->foreignId('course_id')->constrained('course')->after('major_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('classroom', function (Blueprint $table) {
+            $table->foreignId('course_id')->constrained('course')->after('major_id');
+        });
+    }
+};
