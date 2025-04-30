5ab0faa1982f07f4a657c1b0e69d4dd261d7e2eb
WahyuTomo14 <wahyuxd14@gmail.com>
2025-04-30 14:47:46 +0700

    buat table attdance

---
+++ database/migrations/2025_04_30_074358_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDelete;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->string('foto', 255);
+            $table->string('desc', 255)->nullable();
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance');
+    }
+};
