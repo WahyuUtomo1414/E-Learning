23a965b08d9dcd4fb119ce3a9961482763561b1a
WahyuTomo14 <wahyuxd14@gmail.com>
2025-04-30 15:47:30 +0700

    buat pivot table student to classroom

---
+++ app/Models/StudentClassroomsMapping.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StudentClassroomsMapping extends Model
+{
+    use HasFactory;
+    protected $table = 'student_classrooms_mapping';
+
+     // Nonaktifkan timestamps
+    public $timestamps = false;
+
+     // Nonaktifkan fitur primary key karena tabel tidak punya primary key
+    protected $primaryKey = null;
+    public $incrementing = false;
+    
+      // Menambahkan properti fillable
+    protected $fillable = [
+        'student_id',
+        'classroom_id',
+    ];
+    
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'student_id');
+    }
+
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class, 'classroom_id');
+    }
+}
