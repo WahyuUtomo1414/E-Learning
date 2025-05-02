310a17a6b2fb94968f5917e2268be94d18b27bcf
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-02 14:40:47 +0700

    buat manajemen group hak akses di resource role

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -22,7 +22,9 @@ class RoleResource extends Resource
 {
     protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shield-check';
+
+    protected static ?string $navigationGroup = 'Manajemen Hak Akses';
 
     public static function form(Form $form): Form
     {
