c45f40aea76b98aab53b2e804ac79f281b01dcb8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-08 16:34:28 +0700

    buat table day

---
+++ database/migrations/2025_05_08_093246_create_days_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    use BaseModelSoftDelete;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('day', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('day');
+    }
+};
