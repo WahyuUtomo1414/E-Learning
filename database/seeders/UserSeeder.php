4346edd00c93a8fd6b648dadf426594a2e5dafe5
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-08 16:20:27 +0700

    buat seed user baru

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,15 +13,37 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = [
-            "id" => 1,
-            'name' => 'Admin',
-            'email' => "admin@gmail.com",
-            'password' => bcrypt('12345678'),
-            'phone_number' => '08123456789',
-            'avatar_url' => 'default.png',
-            'role_id' => 1,
-            'status_id' => 1,         
+        $user =[
+            [
+                "id" => 1,
+                'name' => 'Admin',
+                'email' => "admin@gmail.com",
+                'password' => bcrypt('12345678'),
+                'phone_number' => '08123456789',
+                'avatar_url' => 'default.png',
+                'role_id' => 1,
+                'status_id' => 1,         
+            ],
+            [
+                "id" => 2,
+                'name' => 'Teacher',
+                'email' => "teacher@gmail.com",
+                'password' => bcrypt('12345678'),
+                'phone_number' => '08123456789',
+                'avatar_url' => 'default.png',
+                'role_id' => 2,
+                'status_id' => 1,      
+            ],
+            [
+                "id" => 3,
+                'name' => 'Student',
+                'email' => "student@gmail.com",
+                'password' => bcrypt('12345678'),
+                'phone_number' => '08123456789',
+                'avatar_url' => 'default.png',
+                'role_id' => 1,
+                'status_id' => 1,      
+            ]
         ];
         User::insert($user);
     }
