51ec17953e34c22136ae8857d2ae1e136bf080aa
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-21 14:57:10 +0700

    buat relsi status di model attendance

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -18,4 +18,9 @@ class Attendance extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
 }
