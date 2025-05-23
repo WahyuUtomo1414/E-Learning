9042abb8c00fcef6c73c33003b2de35beb8e229c
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-23 15:04:42 +0700

    buat layout view

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" href="images/2.png" type="image/png" sizes="16x16">
+        @vite('resources/css/app.css')
+        <title>Attendance</title>
+    </head>
+    <body class="bg-gray-200">
+        {{ $slot }}
+    </body>
+</html>
