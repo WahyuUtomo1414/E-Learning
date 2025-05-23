9547199fb53ddc8f8473de2a012ea972bb38ed64
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-23 14:10:57 +0700

    buat attendance request

---
+++ app/Http/Requests/AttendanceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AttendanceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'latitude' => 'required|string|max:255',
+            'longitude' => 'required|string|max:255',
+            'foto'  => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'desc' => 'nullable|string|max:255',
+            'status_id' => 'required|exists:status,id,status_type_id,2',
+        ];
+    }
+}
