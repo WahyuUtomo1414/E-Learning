8509a158fb8cfe846998f8dc95e6bd4c84973c9f
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-25 23:11:48 +0700

    kasih catatan di halam succes

--- resources/views/pages/succes.blade.php
+++ resources/views/pages/succes.blade.php
@@ -1,15 +1,21 @@
 <x-layouts.base :title="'succes'">
     <div class="max-w-[420px] shadow-lg bg-gradient-to-bl from-white to-sky-100 mx-auto">
+        
+        <!-- buat ngatur tinggi -->
         <div class="py-10"></div>
+
+        <!-- Ilustration -->
         <div class="w-[300px] mx-auto flex justify-center pt-12">
             <img src="{{ asset('images/succes2.png') }}" alt="succes" class="w-full object-cover">
         </div>
 
+        <!-- Text Massage -->
         <div class="text-center font-monospace mt-4 mx-4">
             <h1 class="text-2xl font-semibold py-2 text-sky-800" style="text-shadow: 1px 1px 3px #b3c6ff;">Absensi Berhasil</h1>
             <h3 class="text-sm px-2">Selamat pagi <span class="font-semibold">{{ $user->name }},</span> Data kehadiran Kamu telah dicatat. Silakan lanjutkan aktivitas pembelajaran.</h3>
         </div>
 
+        <!-- Buttom Back -->
         <a href="/admin/attendances">
             <div class="flex justify-center items-center py-10 shadow-lg font-sans">
                 <button class="bg-sky-900 text-white p-2 py-2 rounded-lg hover:bg-sky-800 transition-all duration-200 text-[18px] font-semibold w-[340px]">
