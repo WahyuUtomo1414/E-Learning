21ce9ec64ca0e71740e71a79c5172e20923985d4
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-25 21:41:21 +0700

    masukan ilustrasi gambar succes

--- resources/views/pages/succes.blade.php
+++ resources/views/pages/succes.blade.php
@@ -1,4 +1,8 @@
 <x-layouts.base :title="'succes'">
-    
-
+    <div class="max-w-[420px] h-[800px] shadow-lg bg-white mx-auto">
+        <div class="py-12"></div>
+        <div class="w-[300px] mx-auto flex justify-center pt-12">
+            <img src="{{ asset('images/succes.png') }}" alt="succes" class="w-full object-cover">
+        </div>
+    </div>
 </x-layouts.base>
