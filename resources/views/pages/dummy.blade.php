c8909a5e647c5e43e028f9b5b8e76548bbb90cdb
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-26 15:05:59 +0700

    perbaikan card di halaman dummy

--- resources/views/pages/dummy.blade.php
+++ resources/views/pages/dummy.blade.php
@@ -22,12 +22,43 @@
         </div>
 
         <!-- Card DateTime -->
-        <div class="relative bg-gradient-to-br from-white to-gray-100 text-black max-w-[380px] h-auto mx-auto -mt-6 p-4 rounded-2xl shadow-lg hover:shadow-xl border border-gray-200 justify-items-center">
-            <img src="{{ asset('images/iconabsen.png') }}" class="w-10 h-10 object-cover border border-gray-100" alt="icon absen" />
-            <h1 class="text-xl font-semibold text-center">Absensi Siswa</h1>
-            <h2 class="text-lg text-center">
+        <div class="relative bg-gradient-to-bl from-white to-blue-100 text-black max-w-[calc(100%-2rem)] sm:max-w-[360px] h-auto mx-auto -mt-8 p-5 sm:p-6 rounded-2xl shadow-lg border-gray-200
+            flex flex-col items-center justify-center text-center gap-1 ring-1 ring-sky-700">
+                    
+            <svg fill="#194885" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 610.398 610.398" xml:space="preserve" stroke="#194885"
+                class="w-10 h-10 sm:w-12 sm:h-12 mb-1" {{-- Kelas dari img tag diterapkan di sini --}}
+                role="img" {{-- Tambahan untuk aksesibilitas --}}
+                aria-labelledby="svgTitleIconAbsen">
+                <title id="svgTitleIconAbsen">Ikon Absen</title> {{-- Pengganti alt text untuk aksesibilitas --}}
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier"> <g> <g>
+                    <path d="M159.567,0h-15.329c-1.956,0-3.811,0.411-5.608,0.995c-8.979,2.912-15.616,12.498-15.616,23.997v10.552v27.009v14.052 c0,2.611,0.435,5.078,1.066,7.44c2.702,10.146,10.653,17.552,20.158,17.552h15.329c11.724,0,21.224-11.188,21.224-24.992V62.553 V35.544V24.992C180.791,11.188,171.291,0,159.567,0z"></path>
+                    <path d="M461.288,0h-15.329c-11.724,0-21.224,11.188-21.224,24.992v10.552v27.009v14.052c0,13.804,9.5,24.992,21.224,24.992 h15.329c11.724,0,21.224-11.188,21.224-24.992V62.553V35.544V24.992C482.507,11.188,473.007,0,461.288,0z"></path>
+                    <path d="M539.586,62.553h-37.954v14.052c0,24.327-18.102,44.117-40.349,44.117h-15.329c-22.247,0-40.349-19.79-40.349-44.117 V62.553H199.916v14.052c0,24.327-18.102,44.117-40.349,44.117h-15.329c-22.248,0-40.349-19.79-40.349-44.117V62.553H70.818 c-21.066,0-38.15,16.017-38.15,35.764v476.318c0,19.784,17.083,35.764,38.15,35.764h468.763c21.085,0,38.149-15.984,38.149-35.764 V98.322C577.735,78.575,560.671,62.553,539.586,62.553z M527.757,557.9l-446.502-0.172V173.717h446.502V557.9z"></path>
+                    <path d="M353.017,266.258h117.428c10.193,0,18.437-10.179,18.437-22.759s-8.248-22.759-18.437-22.759H353.017 c-10.193,0-18.437,10.179-18.437,22.759C334.58,256.074,342.823,266.258,353.017,266.258z"></path>
+                    <path d="M353.017,348.467h117.428c10.193,0,18.437-10.179,18.437-22.759c0-12.579-8.248-22.758-18.437-22.758H353.017 c-10.193,0-18.437,10.179-18.437,22.758C334.58,338.288,342.823,348.467,353.017,348.467z"></path>
+                    <path d="M353.017,430.676h117.428c10.193,0,18.437-10.18,18.437-22.759s-8.248-22.759-18.437-22.759H353.017 c-10.193,0-18.437,10.18-18.437,22.759S342.823,430.676,353.017,430.676z"></path>
+                    <path d="M353.017,512.89h117.428c10.193,0,18.437-10.18,18.437-22.759c0-12.58-8.248-22.759-18.437-22.759H353.017 c-10.193,0-18.437,10.179-18.437,22.759C334.58,502.71,342.823,512.89,353.017,512.89z"></path>
+                    <path d="M145.032,266.258H262.46c10.193,0,18.436-10.179,18.436-22.759s-8.248-22.759-18.436-22.759H145.032 c-10.194,0-18.437,10.179-18.437,22.759C126.596,256.074,134.838,266.258,145.032,266.258z"></path>
+                    <path d="M145.032,348.467H262.46c10.193,0,18.436-10.179,18.436-22.759c0-12.579-8.248-22.758-18.436-22.758H145.032 c-10.194,0-18.437,10.179-18.437,22.758C126.596,338.288,134.838,348.467,145.032,348.467z"></path>
+                    <path d="M145.032,430.676H262.46c10.193,0,18.436-10.18,18.436-22.759s-8.248-22.759-18.436-22.759H145.032 c-10.194,0-18.437,10.18-18.437,22.759S134.838,430.676,145.032,430.676z"></path>
+                    <path d="M145.032,512.89H262.46c10.193,0,18.436-10.18,18.436-22.759c0-12.58-8.248-22.759-18.436-22.759H145.032 c-10.194,0-18.437,10.179-18.437,22.759C126.596,502.71,134.838,512.89,145.032,512.89z"></path>
+                </g> </g> </g>
+            </svg>
+            
+            <h1 class="text-lg sm:text-xl font-semibold text-sky-800"> {{-- Warna disesuaikan --}}
+                Absensi Siswa
+            </h1>
+            
+            <p class="text-sm sm:text-base text-gray-600"> {{-- Tag diubah & warna disesuaikan --}}
                 {{ \Carbon\Carbon::now()->locale('id')->translatedFormat('l, d F Y') }}
-            </h2>
-        </div>   
+            </p>
+
+            {{-- Di sini kita akan menambahkan konten yang lebih menarik --}}
+            <div id="dynamic-attendance-content" class="mt-3 w-full">
+                {{-- Konten dinamis akan masuk di sini --}}
+            </div>
+        </div>
     </div>
 </x-layouts.base>
