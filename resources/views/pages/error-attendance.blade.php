fa68348b245b7c63a3d0699a34021cf6abf07f4d
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-26 13:58:12 +0700

    buat massage di halaman eror absensi

--- resources/views/pages/error-attendance.blade.php
+++ resources/views/pages/error-attendance.blade.php
@@ -7,6 +7,20 @@
                 <img src="{{ asset('images/error.png') }}" alt="Ilustrasi absensi berhasil" class="w-full object-cover">
             </div>
 
+            {{--Tulisan Massage --}}
+            <div class="text-center font-inter mx-2 sm:mx-4"> 
+                <h1 class="text-2xl font-semibold py-2 text-sky-800" style="text-shadow: 1px 1px 2px #ffcccc;">
+                    Absensi Gagal
+                </h1>
+                <p class="text-sm px-2 mt-2">
+                    Maaf, <span class="font-semibold">{{ $user->name }}</span>. Absensi anda gagal, silakan hubungi <span class="font-semibold">Admin / Wali Kelas.</span>
+                </p>
+                <p class="text-xs text-gray-600 mt-3">
+                    Pastikan jaringan internet stabil dan berada di area sekolah.
+                </p>
+            </div>
+
+
 
         </div>
     </div>    
