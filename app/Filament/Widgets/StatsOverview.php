054d11d5a3c08b9d4ba092ca8a4d6622dfa879a8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-03 16:11:02 +0700

    ubah tulisan stats widget jadi bahasa inggris

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -22,36 +22,36 @@ class StatsOverview extends BaseWidget
         $user = auth()->user();
 
         return [
-            Stat::make('Tanggal', $tanggal)
+            Stat::make('Date', $tanggal)
                 ->color('success')
                 ->icon('heroicon-s-calendar')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('Selamat Beraktivitas', $jam)
+            Stat::make('Have A Good Day', $jam)
                 ->color('success')
                 ->icon('heroicon-s-bell-alert')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('', 'Absen Masuk')
-                ->description('Klik Untuk Absen Masuk')
+            Stat::make('', 'Attendance')
+                ->description('Click to Attendance')
                 ->descriptionIcon('heroicon-m-arrows-pointing-in', IconPosition::Before)
                 ->extraAttributes([
                     'class' => 'cursor-pointer text-primary font-bold',
-                    'onclick' => "window.location.href='/absenmasuk'",
+                    'onclick' => "window.location.href='/attendance'",
                     'style' => 'background-color: #7AE2CF; color: #077A7D;'
                 ])
                 ->color('white')
                 ->chart([7, 2, 10, 3, 15, 4, 17]),
-            Stat::make('Jumlah Peserta Didik', '242')
-                ->description('Peserta')
+            Stat::make('Student Count', '242')
+                ->description('Student')
                 ->descriptionIcon('heroicon-m-academic-cap')
                 ->chart([2, 3, 35, 18, 15, 26, 15, 30, 25, 30, 25, 50])
                 ->color('success'),
-            Stat::make('Jumlah Guru', '23')
-                ->description('Guru')
+            Stat::make('Teacher Count', '23')
+                ->description('Teacher')
                 ->descriptionIcon('heroicon-m-briefcase')
                 ->chart([32, 23, 35, 18, 15, 56, 15, 30, 25, 30, 25, 30])
                 ->color('warning'),
-            Stat::make('Jumlah Kelas', '17')
-                ->description('Kelas')
+            Stat::make('Class Count', '17')
+                ->description('Class')
                 ->descriptionIcon('heroicon-m-home')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('info'),
