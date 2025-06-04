c4d3de240df679eb957270b4f1b128cde53458f4
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-04 21:37:19 +0700

    buat school seeder

---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\School;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $school = [
+            "id" => 1,
+            "name" => "SMK Patriot Nusantara",
+            "logo" => "images/2.png",
+            "school_master " => 2,
+            "street" => "Jl. Sekolah No.26-27 1, RT.1/RW.5, Kamal, Kec. Kalideres, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11810",
+            "desc" => "",
+            "latitude" => "-6.105245",
+            "longitude" => "106.700657",
+            "school_start_time" => "07:00:00",
+            "status_id" => 1,
+        ];
+        School::insert($school);
+    }
+}
