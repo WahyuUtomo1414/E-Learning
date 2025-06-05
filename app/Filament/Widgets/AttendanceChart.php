87fba32ce7e89d6fe57b6d89d1ddbfd1afc8a273
WahyuTomo14 <wahyuxd14@gmail.com>
2025-06-05 13:17:59 +0700

    perbaikan pie chart widget

--- app/Filament/Widgets/AttendanceChart.php
+++ app/Filament/Widgets/AttendanceChart.php
@@ -2,22 +2,58 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Attendance;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
 
 class AttendanceChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Attendance Chart';
+
+    protected static ?int $sort = 2;
 
     protected function getData(): array
     {
+         // Mengelompokkan attendance berdasarkan status.name
+        // $data = Attendance::select('status.name as status_name', DB::raw('count(*) as total'))
+        //     ->join('status', 'attendance.status_id', '=', 'status.id')
+        //     ->groupBy('status.name')
+        //     ->pluck('total', 'status_name')
+        //     ->toArray();
+        
+        // Dummy data untuk $data
+        $data = [
+            'Hadir' => 40,
+            'Izin' => 10,
+            'Sakit' => 5,
+            'Alpha' => 3,
+        ];
+
+        // filter berdasarkan role user
+        $query = Attendance::query()
+            ->with('user', 'status');
+
+        if (auth()->user()->role_id === 3) {
+            $query->whereHas('user', function ($q) {
+            $q->where('created_by', auth()->id());
+            });
+        }
+
         return [
             'datasets' => [
                 [
-                    'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => array_values($data),
+                    'backgroundColor' => [
+                        '#f87171', // merah
+                        '#34d399', // hijau
+                        '#60a5fa', // biru
+                        '#facc15', // kuning
+                        '#a78bfa', // ungu
+                        '#fb923c', // oranye
+                    ],
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => array_keys($data),
         ];
     }
 
